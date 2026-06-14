23a793a27cc5a292caadc96cfff05ed7627a7a2f
neelabhishek20 <nneelabhishek20@gmail.com>
2026-06-14 14:52:42 +0530

    Create Department_Highest_Salary.sql
    LEET CODE 184

---
+++ Joins/Department_Highest_Salary.sql
@@ -0,0 +1,17 @@
+LEET CODE 184
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN
+(
+    SELECT
+        departmentId,
+        MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
