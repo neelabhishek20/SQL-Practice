fef823177e0e88fb04146c3ce99add05de0685fb
neelabhishek20 <nneelabhishek20@gmail.com>
2026-07-21 10:35:05 +0530

    Time: 1636 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,14 @@
+SELECT
+    DISTINCT p1.product_id,
+    IFNULL(
+        (
+            SELECT new_price
+            FROM Products p2
+            WHERE p2.product_id = p1.product_id
+              AND p2.change_date <= '2019-08-16'
+            ORDER BY change_date DESC
+            LIMIT 1
+        ),
+        10
+    ) AS price
+FROM Products p1;
